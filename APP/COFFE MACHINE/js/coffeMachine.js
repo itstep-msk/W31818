6f82628ae8df373207912930e704847a7d7f9a6b
space2pacman <space2pacman@gmail.com>
2018-09-06 22:00:13 +0300

    add COFFE MACHINE to APP

---
+++ APP/COFFE MACHINE/js/coffeMachine.js
@@ -0,0 +1,84 @@
+	function CoffeMachine(capacity) {
+		var waterAmount = 0;
+		var count = 0;
+		var i;
+		var selectedCoffe;
+		var resolution = false;
+		var typeWaterAmout;
+
+		var typeCoffe = {
+			latte: {
+				waterAmount: 500,
+				count: 1
+			},
+			americano: {
+				waterAmount: 300,
+				count: 5
+			},
+			espresso: {
+				waterAmount: 100,
+				count: 3
+			}
+
+		}
+
+		function onReady() {
+			count--;
+			console.log("Ваш " + selectedCoffe + " будет готов через: " + count);
+			if(count == 0) {
+				clearInterval(i);
+				waterAmount -= typeWaterAmout;
+				console.log("Ваше " + selectedCoffe + " готов.");
+				console.log(waterAmount);
+			}
+			console.log(waterAmount);
+		}
+
+		this.waterAmount = function(amount) {
+			if(amount == undefined) {
+				console.log(waterAmount);
+				return;
+			}
+
+			if(amount <= 0) {
+				console.log("Недопустимое количество воды...");
+				return;
+			}
+
+			if(amount >= capacity) {
+				waterAmount = capacity;
+			} else {
+				waterAmount = amount;
+			}
+		}
+
+		this.changeCoffe = function(type) {
+			selectedCoffe = type;
+			typeWaterAmout = typeCoffe[type].waterAmount;
+			var typeCount = typeCoffe[type].count;
+
+			if (waterAmount < typeWaterAmout) {
+				console.log("Недостаточно воды для выбранного напитка...");
+				return;
+			} else {
+				resolution = true;
+				count = typeCount;
+			}
+		}
+
+		this.run = function() {
+			if(waterAmount <= 0) {
+				console.log("Технический сбой. Ошибка №423573675...");
+				return;
+			}
+			if(resolution) {
+				console.log("Начинаем готовить ваш " + selectedCoffe + "...");
+				i = setInterval(onReady,1000);
+			}
+		}
+	}
+
+	var bosch = new CoffeMachine(1000);
+	//bosch.waterAmount(1000);
+	//bosch.changeCoffe("latte");
+	//bosch.run();
